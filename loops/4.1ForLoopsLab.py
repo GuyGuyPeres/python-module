eadcc96e1dde6bc16d6dfaaed8811c8085abc0d6
CaptainPeres <guyguyperes@gmail.com>
2026-03-29 19:49:09 +0300

    finished for loops exericise

---
+++ loops/4.1ForLoopsLab.py
@@ -0,0 +1,37 @@
+
+# for i in range(1, 6):
+#     print(i)
+
+# for i in range(5, 0, -1):
+#     print(i)
+
+# for i in range(0, 10, 2):
+    # print(i)
+    
+# for i in range(0, 20):
+#     if(i % 3 == 0):
+#         print(i)
+
+# for i in range(10, -1, -1):
+#     print(i)
+
+# my_list = ["guy", "shalev", "adir", "alon"]
+# for name in my_list:
+#     print(name)
+
+# my_list = ["guy", "shalev", "Adir", "Alon"]
+# for name in my_list:
+#     if name.startswith("A"):
+#         print(name)
+
+# numbers_list = [1, 6, 8, 9, 18, 23, 100, 1000, 1003, 136]
+# for number in numbers_list:
+#     if(number % 2 == 0 and number % 3 == 0):
+#         print(number)
+        
+# numbers_list = [1, -6, 8, -9, 18, -23, 100, -1000, 1003, 136]
+# positive_num_list = []
+# for number in numbers_list:
+#     if(number > 0):
+#         positive_num_list.append(number)     
+# print(positive_num_list)
